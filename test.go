bbff83aaa0619bfed5ec50c9e05573efb71629b8
sindreml <sindreml@stud.ntnu.no>
2015-03-02 22:52:30 +0100

    Slenge inn en liden test her på verdensveven

---
+++ test.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+type DataObject struct {
+	newIp int
+	masterQueue []int
+}
+
+func main(){
+	//a := []int {}
+	x := DataObject{}
+	//p := x.masterQueue
+
+	//a := x.newIp
+	fmt.Println(x)
+	//MasterQueue:= [] int {1,2}
+	//b := len(MasterQueue)
+	//fmt.Println(b)
+
+}
